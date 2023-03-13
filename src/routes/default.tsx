526c2bad028a548effac9c28dc884a3ba688f6f3
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-13 14:38:35 -0300

    feat: import button component

--- src/routes/default.tsx
+++ src/routes/default.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Base } from "../template";
+import { Button } from "../components";
 
 const BaseRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Base />}>
-        <Route index element={<h1>Home</h1>} />
+        <Route
+          index
+          element={<Button onClick={() => console.log(false)}>Search</Button>}
+        />
       </Route>
     </Routes>
   );
