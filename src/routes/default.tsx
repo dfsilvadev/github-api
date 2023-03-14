a5b2342c9024ab503a7c7f824ed3bd4c4b5a4a80
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-14 19:37:31 -0300

    feat: import home page

--- src/routes/default.tsx
+++ src/routes/default.tsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Base } from "../template";
-import { Button } from "../components";
+
+import { HomePage } from "../pages";
 
 const BaseRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Base />}>
-        <Route
-          index
-          element={<Button onClick={() => console.log(false)}>Search</Button>}
-        />
+        <Route index element={<HomePage />} />
       </Route>
     </Routes>
   );
