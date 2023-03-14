67f53a9c7a253c7bc65f91ab7125c7bd4560d88e
Daniel Silva <dfsilva.dxp@gmail.com>
2023-03-14 09:09:55 -0300

    feat: add export default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from "./Button";
+export { default as Container } from "./Container";
 export { default as GithubLogo } from "./GithubLogo";
+export { default as SearchBar } from "./SearchBar";
